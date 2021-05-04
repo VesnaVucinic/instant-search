25c259c09e59b4d49f442da2da5df59051fcece6
VesnaVucinic <vesna.vucinic68@gmail.com>
2021-05-04 19:02:14 +0100

    add Hits Widget to retrieve relevant matches based on query

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { InstantSearch } from 'react-instantsearch-dom';
+import { Hits } from 'react-instantsearch-dom';
+
 
 class App extends Component {
   render() {
@@ -9,6 +11,9 @@ class App extends Component {
     apiKey="3d9875e51fbd20c7754e65422f7ce5e1"
     indexName="bestbuy"
   >
+    <main>
+        <Content/>
+    </main>
   </InstantSearch>
     );
   }
